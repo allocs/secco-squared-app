b0573fd3c27aea8c62467e2bc611ec37e70b2590
allocs <alexandracjans@gmail.com>
2026-05-13 08:59:59 -0500

    Added basic supabase integration

--- app/leads/page.tsx
+++ app/leads/page.tsx
@@ -1,4 +1,17 @@
+import { createClient } from "@/lib/supabase/server";
+import { Suspense } from "react";
+
+async function LeadsData() {
+    const supabase = await createClient();
+    const { data: leads } = await supabase.from("leads").select();
+
+    return <pre>{JSON.stringify(leads, null, 2)}</pre>;
+}
+
 export default function Leads() {
-    return <div> Leads page
-    </div>
+    return (
+        <Suspense fallback={<div>Loading Leads...</div>}>
+            <LeadsData />
+        </Suspense>
+    );
 }
