4d9134f39059eafba05780ea87abd212acd6234b
allocs <alexandracjans@gmail.com>
2026-05-13 10:16:01 -0500

    Scaffalding of the lead form

---
+++ app/actions.tsx
@@ -0,0 +1,6 @@
+'use server'
+
+export async function submitLead() {
+    // Figure out if this is something that would be in supaBase or in Next documentation
+
+}
--- components/lead-capturer.tsx
+++ components/lead-capturer.tsx
@@ -1,1 +1,28 @@
 "use client"
+import { useFormStatus } from 'react-dom'
+import Form from 'next/form'
+import { submitLead } from '@/app/actions'
+
+export function SubmitButton() {
+    const status = useFormStatus()
+    return (
+        <button type="submit">{status.pending ? 'Submitting...' : 'Submit'}</button>
+    )
+}
+
+export function LeadCapturer() {
+
+    return (
+        <Form
+            action={submitLead}
+            className="grid"
+        >
+            <input name="name" placeholder="Full Name" />
+            <input name="email" placeholder="Email" />
+            <input name="company" placeholder="Company" />
+            <input name="howdyaHear" placeholder="How Did You Hear About Us?" />
+            <textarea name="message" placeholder="Anything else you want to share?" />
+            <button type="submit">Submit</button>
+        </Form>
+    )
+}
